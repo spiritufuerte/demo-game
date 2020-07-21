37904a0c6f18040595bb8feae4d334dbc1b0b609
Vasyl Biliba <58170018+spiritufuerte@users.noreply.github.com>
2020-07-21 19:46:33 +0300

    Updates

---
+++ precache-manifest.9687b1e716fb9a3dc12d44b6ad81bc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bc4816d0725539f2afb2d78fb7ecad",
+    "url": "/demo-game/index.html"
+  },
+  {
+    "revision": "36c1d720f33509fa427d",
+    "url": "/demo-game/static/css/main.8df0c8ba.chunk.css"
+  },
+  {
+    "revision": "a955bda3a018379f6ce1",
+    "url": "/demo-game/static/js/2.c9a02b05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-game/static/js/2.c9a02b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c1d720f33509fa427d",
+    "url": "/demo-game/static/js/main.2034385b.chunk.js"
+  },
+  {
+    "revision": "4564893e88fe0f96d368",
+    "url": "/demo-game/static/js/runtime-main.b94e98cd.js"
+  }
+]);
